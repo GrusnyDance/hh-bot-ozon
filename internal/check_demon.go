010c2345ca1170510c51477787ea87c082779118
GrusnyDance <grusny.dance@gmail.com>
2023-01-29 19:53:46 +0100

    minor fix

--- internal/check_demon.go
+++ internal/check_demon.go
@@ -1,17 +1,14 @@
 package internal
 
 import (
-	"fmt"
 	"hh-bot-ozon/repository"
 	"time"
 )
 
 func CheckUpdates(i *repository.Instance, ch chan []string) {
 	for { //собираем данные
-		fmt.Println("I work")
 		mapVac := GetOpenPositions()
 		if len(*mapVac) > 0 {
-			fmt.Println("i am line 16")
 			if resMap, ok := i.GetVac(mapVac); ok {
 				for k, v := range *resMap {
 					ret := []string{k, v}
